cf888291800e082c68b10b250b779b49c8a8dd75
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-07-05 05:30:11 -0400

    added style and tweaked genre behaviour

--- scripts/directives/appDirectives.js
+++ scripts/directives/appDirectives.js
@@ -1,12 +1,13 @@
-app.directive('ngEnter', function () {//directive for enter key
-    return function (scope, element, attrs) {
-        element.bind("keydown keypress", function (event) {
-            if(event.which === 13) {
-                scope.$apply(function (){
-                    scope.$eval(attrs.ngEnter);
-                });
-                event.preventDefault();
-            }
-        });
-    };
-});
+angular.module('appDirectiveModule', [])
+    .directive('ngEnter', function () {//directive for enter key
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if(event.which === 13) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    });
