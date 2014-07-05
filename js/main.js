d0ea8b7edee7d52a5b1507a1973b1e52695a1a10
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-07-04 20:17:46 -0400

    Fixed enter key and genre list

--- js/main.js
+++ js/main.js
@@ -37,15 +37,14 @@ app.directive('ngEnter', function () {
         });
     };
 });
-
-	$scope.submitSearch = function(genre) {//api call for the query
+	$scope.submitSearch = function(genre, id) {//api call for the query/genre
 	  var queryText = $scope.searchText;
 	  if (genre == undefined) {
 	  	var genre = $scope.searchText;
-	  	var searchUrl = 'https://gdata.youtube.com/feeds/api/videos?q='+queryText.split(' ').join('+')+'+trailer&v=2&max-results=5&hd=true&alt=json&category=Trailer&callback=JSON_CALLBACK';
+	  	var searchUrl = 'https://gdata.youtube.com/feeds/api/videos?q='+queryText.split(' ').join('+')+'+trailer/MOVIE-TITLE&v=2&max-results=5&hd=true&alt=json&category=Trailer&callback=JSON_CALLBACK';
 	  }
 	  else {
-	 	var searchUrl = 'https://gdata.youtube.com/feeds/api/videos?q='+genre.split(' ').join('+')+'+trailer&v=2&max-results=5&hd=true&alt=json&category=Trailer&callback=JSON_CALLBACK';
+	  		var searchUrl = 'https://gdata.youtube.com/feeds/api/videos?q='+genre.split(' ').join('+')+'+trailer+/MOVIE-GENRE&v=2&orderby=viewCount&max-results=5&hd=true&alt=json&category='+genre.split(' ').join('+')+'&callback=JSON_CALLBACK';
 	  }
 	$http({
 		method: 'JSONP',
