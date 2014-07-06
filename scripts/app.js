cf285bdb53169a50d0df7b7ae6d401284dc93ed9
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-07-06 14:29:57 -0400

    update

--- scripts/app.js
+++
@@ -1,3 +0,0 @@
-var app = angular.module('myApp', ['appFiltersModule', 'appControllerModule', 'appDirectiveModule']); //creates a new module called myApp
-var apiKey = 'AIzaSyDYhqH1guvlxxocuttrwxE2kkvYefu0cqo'; //api key for youtube
-var searchInput; //records a user input
