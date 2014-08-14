836ffc57ce10dc8cd5f38db1f177cb9d6ed5cff3
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-08-14 18:59:34 -0400

    Organized code modularly, used Angular best prac

---
+++ scripts/appDirectives.js
@@ -0,0 +1,15 @@
+(function() {
+    var app = angular.module('appDirectiveModule', []);
+        app.directive('ngEnter', function () {//directive for enter key
+            return function (scope, element, attrs) {
+                element.bind("keydown keypress", function (event) {
+                    if(event.which === 13) {
+                        scope.$apply(function (){
+                            scope.$eval(attrs.ngEnter);
+                        });
+                        event.preventDefault();
+                    }
+                });
+            };
+        });
+})();
---
+++ scripts/appFilters.js
@@ -0,0 +1,8 @@
+(function() {
+	var app = angular.module('appFiltersModule', []);
+		app.filter('capitalize', function() {
+	  		return function(input) {
+	  			return input[0].toUpperCase() + input.slice(1);
+	  	}  
+	});
+})();
