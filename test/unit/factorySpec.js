8e327d0af4186f25e797cb661a611b9027051884
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-08-17 20:35:55 -0400

    Unit Tests Upadted

--- test/unit/factorySpec.js
+++ test/unit/factorySpec.js
@@ -2,9 +2,10 @@
 describe('factory tests', function() {
 	beforeEach(module('appFactoriesModule'));
 
-	describe('youtubeApiService', function(){
-		it('should connect to the youtube api', inject(function(youtubeApiService){
+	describe('Make sure the http calls work', function(){
+		it('should connect to the youtube api', inject(function(youtubeApiService, rottenTomatoesService){
 			expect(youtubeApiService.getVideos('https://gdata.youtube.com/feeds/api/videos?q=the+lion+king+official+trailer&v=2&max-results=3&alt=json&category=movies&callback=JSON_CALLBACK')).toBeDefined();
+			expect(rottenTomatoesService.getRatings("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=4cwjthjq2hyxz7amh6jj5p4p&q=this+is+it&page_limit=5&callback=JSON_CALLBACK")).toBeDefined();
 		}));
 	});
 });
