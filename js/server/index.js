c0486147cb2e336b0b8ab603426de27ae4b52aaa
pedromcunha <pedro.spinsomewebs@gmail.com>
2014-09-22 01:27:19 -0400

    created a node server on port 5k

---
+++ js/server/index.js
@@ -0,0 +1,56 @@
+//The Server
+var express = require('express'),
+    // exphbs  = require('express3-handlebars'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    TwitterStrategy = require('passport-twitter'),
+    GoolgeStrategy = require('passport-google'),
+    FacebookStrategy = require('passport-facebook');
+
+var config = require('./config.js'), //config file contains all tokens and other private info
+    funct = require('./functions.js'); //funct file contains our helper functions for our Passport and database work
+
+var app = express();
+
+//===============PASSPORT===============
+
+//Passports go in here
+
+//===============EXPRESS================
+//Express config
+
+app.use(express.logger());
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'supernova' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next){
+  var err = req.session.error,
+      msg = req.session.notice,
+      success = req.session.success;
+
+  delete req.session.error;
+  delete req.session.success;
+  delete req.session.notice;
+
+  if (err) res.locals.error = err;
+  if (msg) res.locals.notice = msg;
+  if (success) res.locals.success = success;
+
+  next();
+});
+
+
+//===============ROUTES===============
+
+//routes go in here, if needed. Shouldn't be required
+
+//===============PORT=================
+var port = process.env.PORT || 5000; //select your port or let it pull from your .env file
+app.listen(port);
+console.log("listening on " + port + "!");
+
+
